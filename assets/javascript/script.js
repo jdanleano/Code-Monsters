975852cc592ce7c8e87f64322b64460bd1ddd72c
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-07-12 21:03:26 -0400

    Quickly wrote down tasks
    HTML, CSS and JS have comments for each task that needs to be done.

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+// Access Youtube API to submit searches for content we want to show (ex. Javascript videos with a certain rating)
+
+
+// Access Youtube API for User searches
+
+
+// Implement Firebase API to allow users to chat and potentially handling log in for site
+
+
+// Declare any necessary variables
+
+
+// Add Event Delegates or listeners for the for each section of videos
+
+
+// Local Storage for Favorites, Recently Viewed, Notes, Chat History
+
+
